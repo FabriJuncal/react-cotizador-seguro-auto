50288823b8d11055e3af3ddec09d0dbe387a6220
fjuncal <fjuncal@tdi.com.ar>
2021-03-03 22:17:07 -0300

    89. Agregando Emotion para Styled Components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,30 @@
+import styled from '@emotion/styled'
+
+// Importamos la libreria "Styled Components"
+// Se utiliza para mesclar Js con Css
+// Lo que se hace es declarar a una variable una funcíon con las deficiniones Css en un Template String
+// Y luego utilizar los nombres de las variables en forma de etiquetas HTML
+
+const ContenedorHeader = styled.header`
+    background-color: #26C6DA;
+    padding: 10px;
+    font-weight: bold;
+    color: #FFFFFF;
+`;
+
+const TextoHeader = styled.h1`
+    font-size: 2rem;
+    margin: 0;
+    font-family: 'Slabo 27px', serif;
+    text-align: center;
+`;
+
 const Header = ({titulo}) => ( 
-    <header>
-        <h1>{titulo}</h1>
-    </header> 
+    // Utilizamos los Styled Components Creados
+    // Para utilizarlos se agrega el nombre de las variables con la función de "Emotion" como si fueran etiquetas HTML
+    <ContenedorHeader> 
+        <TextoHeader>{titulo}</TextoHeader>
+    </ContenedorHeader> 
 
 );
 
